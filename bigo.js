2be48aaf114fa4e70665730cad61bf865675418c
Abhisoni0398 <abhisoni0398@gmail.com>
2021-01-20 18:40:29 +0530

    initial commit

---
+++ bigo.js
@@ -0,0 +1,9 @@
+const large=new Array(100).fill("nemo");
+function findNemo(array){
+    for (let i=0;i<=array.length;i++){
+        if (array[i]==='nemo'){
+            console.log("found nemo");
+        }
+    }
+}
+findNemo(large);
