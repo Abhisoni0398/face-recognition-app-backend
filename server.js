1a04370ee8b4dca611aef7856d451f367eff9b0a
Abhisoni0398 <abhisoni0398@gmail.com>
2021-01-23 11:15:33 +0530

    hehe

--- server.js
+++ server.js
@@ -4,14 +4,14 @@ const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex')
 
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
 const db = knex({
+  // Enter your own database information here based on what you created
   client: 'pg',
   connection: {
-    connectionString:process.env.process.env.postgres://uajvalsbcntxjy:7485add35d2cb255ab1d6b9782f087e86b078201a9c59ae5ee70297e090f14c5@ec2-54-146-91-153.compute-1.amazonaws.com:5432/d9q9pu4e8gpi3q,
-    ssl:{
-      rejectUnauthorized: false
-    }
+    host : '127.0.0.1',
+    user : 'aneagoie',
+    password : '',
+    database : 'smart-brain'
   }
 });
 
@@ -21,15 +21,13 @@ app.use(cors())
 app.use(bodyParser.json());
 
 app.get('/', (req, res)=> {
-  res.send("Its Working!");
+  res.send(database.users);
 })
 
 app.post('/signin', (req, res) => {
-    console.log(req)
   db.select('email', 'hash').from('login')
     .where('email', '=', req.body.email)
     .then(data => {
-        console.log(data)
       const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
       if (isValid) {
         return db.select('*').from('users')
@@ -97,12 +95,6 @@ app.put('/image', (req, res) => {
   .catch(err => res.status(400).json('unable to get entries'))
 })
 
-app.use(function(req,res,next){
-  res.header('Access-Control-Allow-Origin',"*");
-  res.header('Access-Control-Allow-Origin',"GET,PUT,POST,DELETE");
-  res.header('Access-Control-Allow-Origin',"Content-Type");
-})
-
-app.listen(process.env.PORT || 3000, ()=> {
-  console.log(`app is running on port ${process.env.PORT}`);
+app.listen(3000, ()=> {
+  console.log('app is running on port 3000');
 })
